1acbf777feaa261b1d0a0a8ff1cdd61ac20fb2ca
Pawandeep123 <Pawandeepgrewal1990@gmail.com>
2020-06-23 10:41:31 +0530

    Gatsby recaptcha

--- src/components/Contactform/index.js
+++ src/components/Contactform/index.js
@@ -27,12 +27,12 @@ render() {
                netlify ="true"
                data-netlify-recaptcha="true"
                data-netlify="true" 
-              //  data-netlify-honeypot="bot-field"
+               data-netlify-honeypot="bot-field"
                className="form"
             >
             <Row>
               <Col md="6" data-sal-duration="1000" data-sal="slide-up" data-sal-delay="100" data-sal-easing="ease-out-bounce">
-                {/* <input type="hidden" name="form-name" value="contact" /> */}
+                <input type="hidden" name="form-name" value="contact" />
                 <Form.Group controlId="formBasicEmail" >
                     <Form.Label htmlFor="First Name">First Name <sup>*</sup></Form.Label>
                     <Form.Control type="text" name="firstname" placeholder="First Name" />
