c18f51e0cfb298270d0e86ffbd38ca6e38ebcd4a
Pawandeep123 <Pawandeepgrewal1990@gmail.com>
2020-06-23 11:23:05 +0530

    contact form

--- src/components/Contactform/index.js
+++ src/components/Contactform/index.js
@@ -2,89 +2,117 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Form, Button} from 'react-bootstrap'
+// import { Container, Row, Col, Form, Button} from 'react-bootstrap'
 import "./contactform.css"
 import sal from 'sal.js'
+import { navigateTo } from "gatsby-link";
+import Recaptcha from "react-google-recaptcha";
 import  '../sal.css';
   sal();
+  const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
 
-class ContactformrData extends React.Component {
+  function encode(data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  }
+  export default class Contactform extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleRecaptcha = value => {
+    this.setState({ "g-recaptcha-response": value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
 render() {
-  const { data } = this.props
-  const { contentfulContact } = data
+
   return (
-      <div className="Contactform">
-          <Container>
-            <Row>
-              <Col sm="12" data-sal-duration="1000" data-sal="slide-up"  data-sal-easing="ease-out-bounce">
-                  <h4 className="h2 mb-2">{contentfulContact.anyQuestion}</h4>
-                  <span>{contentfulContact.childContentfulContactTextAreaTextNode.textArea}</span>
-              </Col>
-            </Row>
-            <Form 
-               name="contact" 
-               method="POST" 
-               netlify ="true"
-               data-netlify-recaptcha="true"
-               data-netlify="true" 
-               data-netlify-honeypot="bot-field"
-               className="form"
-            >
-            <Row>
-              <Col md="6" data-sal-duration="1000" data-sal="slide-up" data-sal-delay="100" data-sal-easing="ease-out-bounce">
-                <input type="hidden" name="form-name" value="contact" />
-                <Form.Group controlId="formBasicEmail" >
-                    <Form.Label htmlFor="First Name">First Name <sup>*</sup></Form.Label>
-                    <Form.Control type="text" name="firstname" placeholder="First Name" />
-                  </Form.Group>
-                  <Form.Group controlId="formBasicEmail">
-                    <Form.Label htmlFor="Last Name">Last Name <sup>*</sup></Form.Label>
-                    <Form.Control type="tex" name="lastname" placeholder="Last Name" />
-                  </Form.Group>
-                  <Form.Group controlId="formBasicEmail">
-                    <Form.Label htmlFor="Email">Email <sup>*</sup></Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Email Address" />
-                  </Form.Group>
-              </Col>
-              <Col md="6" data-sal-duration="1000" data-sal="slide-up" data-sal-delay="200" data-sal-easing="ease-out-bounce">
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label htmlFor="Message">Message <sup>*</sup></Form.Label>
-                    <Form.Control as="textarea" name="message" />
-                  </Form.Group>
-              </Col>
-              <div className="col-12" data-netlify-recaptcha="true"></div>
-              <Col data-sal-duration="900" data-sal="slide-up" data-sal-delay="300" data-sal-easing="ease-out-bounce">
-                  <Button type="submit" className="button">
-                    Submit
-                  </Button>
-              </Col>
-            </Row>
-            </Form>
-          </Container>
+      <div className="Contactform container">
+                <div>
+        <h1>reCAPTCHA 2</h1>
+        <form
+          name="contact-recaptcha"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-recaptcha="true"
+          onSubmit={this.handleSubmit}
+        >
+          <noscript>
+            <p>This form won’t work with Javascript disabled</p>
+          </noscript>
+          <p>
+            <label>
+              Your name:<br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
+          <Recaptcha
+            ref="recaptcha"
+            sitekey={"6LeHT6gZAAAAAC88WSw7jF7k97sillbbMOrwWgco"}
+            onChange={this.handleRecaptcha}
+          />
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
       </div>
 
     )
   }
 }
-ContactformrData.propTypes = {
-  data: PropTypes.object,
-}
+// ContactformrData.propTypes = {
+//   data: PropTypes.object,
+// }
 
-export default function Contactform(props) {
-  return (
-    <StaticQuery
-      query={graphql`
-          query ContactformQuery {
-            contentfulContact {
-              anyQuestion
-              childContentfulContactTextAreaTextNode {
-                textArea
-              }
-            }
-          }
-      `}
-      render={data => <ContactformrData data={data} {...props} />}
-    />
-  )
-}
+// export default function Contactform(props) {
+//   return (
+//     <StaticQuery
+//       query={graphql`
+//           query ContactformQuery {
+//             contentfulContact {
+//               anyQuestion
+//               childContentfulContactTextAreaTextNode {
+//                 textArea
+//               }
+//             }
+//           }
+//       `}
+//       render={data => <ContactformrData data={data} {...props} />}
+//     />
+//   )
+// }
 
