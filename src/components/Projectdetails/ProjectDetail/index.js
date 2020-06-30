1e8c9096e13c3cfa58c39f64eb31104ba1b167fe
Pawandeep123 <Pawandeepgrewal1990@gmail.com>
2020-06-30 18:21:36 +0530

    fix product page layout

--- src/components/Projectdetails/ProjectDetail/index.js
+++ src/components/Projectdetails/ProjectDetail/index.js
@@ -59,7 +59,7 @@ class ProjectDetail extends React.Component {
                <Container>
                  <Row>
                     <Col>
-                       <h4 className="h3 mb-4" data-sal-duration="1300" data-sal="slide-up" data-sal-delay="300" data-sal-easing="ease-out-expo">
+                       <h4 className="h3 mb-4 pl-lg-5 pr-lg-5" data-sal-duration="1300" data-sal="slide-up" data-sal-delay="300" data-sal-easing="ease-out-expo">
                          <div dangerouslySetInnerHTML={{ __html: data.childContentfulProjectCovid19BlockDescriptionRichTextNode.childContentfulRichText.html }} />
                        </h4>
                        <Link to="/contact" className="button mt-4 " data-sal-duration="1300" data-sal="slide-up" data-sal-delay="400" data-sal-easing="ease-out-expo">Schedule a Demo &nbsp;&nbsp;<FontAwesomeIcon icon="play-circle" /></Link>
